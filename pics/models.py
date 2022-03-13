87f81f73c276451410d344d9aea9305f75a56069
charliesheh <59520443+charliesheh@users.noreply.github.com>
2022-03-13 16:16:39 -0700

    Update models.py

--- pics/models.py
+++ pics/models.py
@@ -22,3 +22,12 @@ class Pic(models.Model):
     # Shows up in the admin list
     def __str__(self):
         return self.title
+    
+    #Assignment 1
+    class Ad(models.Model) :
+
+    ...
+    # Picture
+    picture = models.BinaryField(null=True, editable=True)
+    content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
+    ...
